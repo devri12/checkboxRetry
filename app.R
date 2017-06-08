1a9433aea6a97289b8bb47ce88e80a67a4ca6843
Devri Adams <devri.adams@duke.edu>
2017-06-08 14:47:55 -0400

    Update of watershed practice graphs

--- app.R
+++ app.R
@@ -6,6 +6,8 @@ library(tidyr)
 library(lubridate)
 library(magrittr)
 library(plotly)
+library(dygraphs)
+library(xts)
 
 w1_precip_chem <- read_csv("D:/Duke/Work(Environ)/Data/w1_precip_chem.txt")
 w1_stream_chem <- read_csv("D:/Duke/Work(Environ)/Data/w1_stream_chem.txt")
@@ -624,11 +626,16 @@ pH.flowyear.6 <- group_by(w6_chem, year) %>%
 #combine pH flowyear and precipyear
 pHdata.6 <- merge(pH.precipyear.6, pH.flowyear.6, by = "year")
 
+#combine all watershed 2 ions (Ca, NO3, SO4, pH)
+CaNO2 <- merge(Cadata.2, NO3data.2, by = "year")
+SOp2 <- merge(SO4data.2, pHdata.2, by = "year")
+all2 <- merge(CaNO2, SOp2, by = "year")
+
 #possibly change all dates (Cadata, NO3data, SO4data, pHdata) to y/m/d format
 #ws2
 #change back to real date
 w2_chemre <- unite(w2_chem, date, year:day, sep = "/")
-as.Date(w2_chemre$date) %m+% months (5)
+w2_chemre$date <- as.Date(w2_chemre$date) %m+% months (5)
 #combine ionflow columns into one
 #w2_chemre <- w2_chemre %>%
 #  gather(ionflow, value, Ca.flow:SiO2.flow) %>%
@@ -636,14 +643,44 @@ as.Date(w2_chemre$date) %m+% months (5)
 #  group_by(date) %>%
 #  summarise_each(funs(paste(unique(.), collapse = "\n")))
 
+#ws5
+#change back to real date
+w5_chemre <- unite(w5_chem, date, year:day, sep = "/")
+w5_chemre$date <- as.Date(w5_chemre$date) %m+% months (5)
+
+#ws6
+#change back to real date
+w6_chemre <- unite(w6_chem, date, year:day, sep = "/")
+w6_chemre$date <- as.Date(w6_chemre$date) %m+% months (5)
+#make two df to compare precip and flow
+w6pHvs <- w6_chemre[, c(2:3, 16)]
+#select date and precip
+w6pHvsP <- select(w6pHvs, date, precip)
+#add column of "precip"
+w6pHvsP$source <- rep("precip", ncol(w6pHvsP))
+#rename precip
+w6pHvsP$value <- w6pHvsP$precip
+w6pHvsP$precip <- NULL
+#select date and flow
+w6pHvsQ <- select(w6pHvs, date, flow)
+#add column of "flow"
+w6pHvsQ$source <- rep("flow", ncol(w6pHvsQ))
+#rename flow
+w6pHvsQ$value <- w6pHvsQ$flow
+w6pHvsQ$flow <- NULL
+#use rbind to combine date and precip df
+w6pHvsF <- rbind(w6pHvsP, w6pHvsQ)
+
 
 #choose which variables to graph, with each tab a different watershed perhaps
-ui <- fluidPage(
-  titlePanel(("Hopeful")),
+ui <- fluidPage( theme = "Solar_bootstrap.min.css",
+  titlePanel(("Hopeful practice")),
   
   sidebarLayout(
     sidebarPanel(
-      
+      em("Each watershed is currently represented by a different interactive variation
+        of exploration. Ideally, functions will be made to clean up the code 
+        backing them.")
     ),
     mainPanel(
       tabsetPanel(
@@ -671,17 +708,36 @@ ui <- fluidPage(
                              label = "Choose one",
                              selected = "Ca.flow",
                              choices = list("Calcium discharge" = "Ca.flow",
-                                            "Calcium precipitation" = "Ca.precip")),
-                 plotOutput("c2")),
+                                            "Calcium precipitation" = "Ca.precip",
+                                            "NO3 discharge" = "NO3.flow",
+                                            "NO3 precipitation" = "NO3.precip",
+                                            "SO4 discharge" = "SO4.flow",
+                                            "SO4 precipitation" = "SO4.precip",
+                                            "pH discharge" = "pH.flow",
+                                            "pH precipitation" = "pH.precip")),
+                 plotOutput("c2"),
+                 br(),
+                 plotOutput("all2")),
         tabPanel("Watershed 3",
                  checkboxGroupInput("ws3",
-                                    label = "Choose one compounds",
+                                    label = "Choose one compound, to be continued...",
                                     selected = "Ca.flow",
                                     choices = list("Calcium discharge" = "Ca.flow",
                                                    "Calcium precipitation" = "Ca.precip")),
                  plotOutput("c3")),
         tabPanel("Watershed 4",
-                 plotOutput("c4"))
+                 plotOutput("c4")),
+        tabPanel("Watershed 5",
+                 tabsetPanel(
+                   tabPanel("Monthly precipitation",
+                            plotOutput("P5")),
+                 tabPanel("Monthly streamflow",
+                          plotOutput("Q5"))
+                 )
+        ),
+        tabPanel("Watershed 6",
+                 h3("Precipitation pH vs Streamflow pH"),
+                 plotOutput("pHvs6"))
       )
     )
   )
@@ -693,34 +749,60 @@ server <- function(input, output) {
     #plot the data interactively
     ggplot(Cadata.1, aes(x = as.Date(year), y = get(input$cws1), col = input$cws1)) +
       labs(colour = "Analytes", x = "Year", y = "ueq/L") +
-      geom_point()+
+      geom_line()+
       xlim(min(input$slide1[1]), max(input$slide1[2]))
   })
   output$s1 <- renderPlot({
     ggplot(SO4data.1, aes(x = as.Date(year), y = get(input$sws1), col = input$sws1)) +
-      geom_jitter()+
+      geom_line()+
       xlim(min(input$slide1[1]), max(input$slide1[2]))
   })
-#  output$all2 <- renderPlot({
- #   ggplot(w2_chemre, aes(x = date, y = compounds)) +
-  #    geom_tile()
-#  })
   output$c2 <- renderPlot({
-    ggplot(Cadata.2, aes(x = year, y = get(input$ws2), col = input$ws2)) +
+    ggplot(all2, aes(x = year, y = get(input$ws2), col = input$ws2)) +
       labs(colour = "Ion", x = "Year", y = "ueq/L") +
       geom_point()
   })
+  output$all2 <- renderPlot({
+    ggplot(all2, aes(x = year)) +
+      geom_line(mapping = aes(x = as.numeric(year), y = get(input$ws2)))
+    #      geom_tile()
+  })
   output$c3 <- renderPlot({
-    ggplot(Cadata.3, aes(x = year, y = get(input$ws3), col = input$ws3)) +
+    ggplot(Cadata.3, aes(x = as.numeric(year),  col = input$ws3)) +
       labs(colour = "Ion", x = "Year", y = "ueq/L") +
-      geom_point()
+      geom_line() 
+#      if(input$ws3[1] = T
 #      geom_point(aes(y = get(input$ws3[1]))) +
 #      geom_point(aes(y = get(input$ws3[2])))
+#      )
   })
   output$c4 <- renderPlot({
     c4 <- ggplot(Cadata.4, aes(x = year, y = Ca.precip, col = Ca.precip)) +
       geom_point()
     ggplotly(c4)
+  })
+  output$P5 <- renderPlot({
+    #plot monthly precipitation
+    xts(w5_chemre$precip, order.by =w5_chemre$date) %>%
+      dygraph(., main = "Monthly precip", xlab = "Month", ylab = "Precip (cm)") %>%
+      dyAxis("x", drawGrid = F)
+    #option to make it yearly?
+  })
+  output$Q5 <- renderPlot({
+    #plot monthly streamflow
+    ggplot(w5_chemre, aes(x = date, y = flow))+
+      geom_line() 
+    #option to make it yearly?
+  })
+  output$pHvs6 <- renderPlot({
+    ggplot(w6_chemre, aes(x = date)) +
+      geom_line(aes(y = flow, col = "flow")) +
+      geom_line(aes(y = precip, col = "precip"))
+  })
+  output$pHvs6dy <- renderPlot({
+    group_by(w6pHvsF$source) %>%
+    xts(w6pHvsF$value, order.by = w6pHvsF$date) %>%
+      dygraph(., main = "Monthly") 
   })
 }
 
